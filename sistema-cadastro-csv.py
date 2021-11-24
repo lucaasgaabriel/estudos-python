ad3d779173b4027b7e675faff531e515295cf3a6
Lucas Marques <lucaas.gaabriel@live.com>
2021-11-24 11:29:10 -0300

    Update sistema-cadastro-csv.py

--- sistema-cadastro-csv.py
+++ sistema-cadastro-csv.py
@@ -6,16 +6,16 @@ while opt != '0':
     nome = input('Digite o nome: ')
     idade = input('Digite a idade: ')
     dados.append([nome, idade])
-    opt = input('O que deseja fazer?\n 1- Cadastrar\n 0- Sair\n')
+    opt = input('O que deseja fazer?\n1- Cadastrar\n 0- Sair\n')
     
 print(dados)
 
 with open('users.csv', 'w', newline ='') as arquivo_csv:
     writer = csv.writer(arquivo_csv)
     writer.writerow(header)
-    writer.writerow(dados)
+    writer.writerows(dados)
     
 with open('users.csv', 'r') as csv_file:
-    csv.reader = csv.reader(csv_file, delimiter = ',')
+    csv.reader = csv.reader(csv_file, delimiter =',')
     for row in csv_reader:
         print(row)
