a71e49b7a69065a4528b84e279b809e4c5d477e0
Lucas Marques <lucaas.gaabriel@live.com>
2022-02-14 23:35:24 -0300

    Loop with time execution
    This loop can use a number from the user's writing. In the function the number written by the user will be displayed and at the end it will show what was the execution time of the code

---
+++ countloop-with-time-execution.py
@@ -0,0 +1,25 @@
+import time
+import timeit
+
+print("=====================")
+print("----Hello World------")
+print("=====================")
+
+contador = 1
+max_contador = int(input("Digite um número para fazer a contagem: "))
+
+def timer():
+    for i in range(1, max_contador):
+        time.sleep(1)
+
+inicio = timeit.default_timer()
+
+while (max_contador >= contador):
+    print(contador)
+
+    contador += 1
+
+
+fim = timeit.default_timer()
+print ('duracao: %f' % (fim - inicio))
+print("_________")
