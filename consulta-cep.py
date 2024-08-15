86833e711f3f0f9135db9cf170297da889b5224a
Lucas Marques <lucas.marques@poder360.com.br>
2024-08-14 23:09:55 -0300

    Feat: adição de arquivo consulta-cep.py

---
+++ consulta-cep.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+ceps = [''] #Digite aqui quais CEPs devem ser consultados
+
+def busca_cep(cep):
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+
+    return requests.get(url).json()
+
+
+for cep in ceps:
+    response = busca_cep(cep)
+    time.sleep(0.5) #Timeout entre uma consulta e outra para não sobrecarregar a API
+    uf = response.get('uf')
+
+    if uf == 'DF':
+        print(f'Esse CEP está localizado em {response.get('localidade')} - {response.get('uf')}')
+    else:
+        print(f'Esse CEP está localizado em {response.get('localidade')} - {response.get('uf')}')
+
+
+
